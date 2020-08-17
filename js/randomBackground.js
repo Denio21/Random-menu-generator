aaadef515d8498fe9e62a5c9fdc1e51e827c955e
Denis <lenger.denis@seznam.cz>
2020-08-17 14:29:23 +0200

    Added footer and changed colors

--- js/randomBackground.js
+++ js/randomBackground.js
@@ -1,9 +1,17 @@
 function random_bg_color() {
     var colors = [
-        "#e8505b",
-        "#f9d56e",
-        "#f3ecc2",
-        "#14b1ab"
+        "#fc5c65",
+        "#fd9644",
+        "#fed330",
+        "#26de81",
+        "#2bcbba",
+        "#fa8231",
+        "#f7b731",
+        "#20bf6b",
+        "#0fb9b1",
+        "#45aaf2",
+        "#4b7bec",
+        "#2d98da"
     ]
     var metaThemeColor = document.querySelector("meta[name=theme-color]");
     var bgColor = colors[Math.floor(Math.random() * colors.length)];
